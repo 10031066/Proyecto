d006db8ab8e8e73c8ca8bedf450cdcf8fdd97e3b
karlit <karlita_erika_07_14@hotmail.com>
2015-06-02 15:15:43 -0500

    devolucion.php

---
+++ devolucion.php
@@ -0,0 +1,147 @@
+<?php
+ include 'top.php'
+?>
+
+	<div class="row">
+		<div class="col-md-2">
+      <img src="img/public.png" width="300" />
+      <img src="img/public1.png" width="300" />
+      <br>
+      <img src="img/public2.png" width="300" />
+
+
+      </div>
+
+      <div class="col-md-8">
+
+         <h3><font color="#DF3A01">¿Como devuelvo un producto?</font></h3>
+        <br>
+
+           <h4><font color="#585858"><p align="justify">Haz la devolución de tu producto ingresando directamente a tu cuenta en Muebles el Carpintero. Recuerda que tienes 5 días hábiles a partir de la fecha en que lo recibiste para realizar la devolución ¡sin ningún costo!</P>
+           </font></h4>
+           <br>
+
+           <h3><font color="#DF3A01">¿Puedo devolver todos los productos?</font></h3>
+           <br>
+
+           <h4><font color="#585858"><p align="justify">No, debido a la naturaleza de algunos artículos, no aceptamos devoluciones en todos los productos.
+
+          Asimismo, debes tener en cuenta que cualquier producto con un costo mayor o igual a $20,000.00 únicamente podrá ser devuelto por presentar algún daño o falla en su funcionamiento. En caso de que el producto no haya cumplido con tus expectativas, desafortunadamente no podremos aceptar su devolución.</P>
+           </font></h4>
+
+           <br>
+
+           <h3><font color="#DF3A01">¿Puedo cambiar un producto?</font></h3>
+           <br>
+           <h4><font color="#585858"><p align="justify">No, en Linio no realizamos cambios; si el producto que ordenaste no cumple con tus expectativas, lo único que tienes que hacer es devolverlo.
+
+            Si deseas que te enviemos un producto igual o de diferentes características, nosotros nos encargaremos de generar una nueva orden.
+
+            No olvides que, por cuestiones de higiene y calidad, existe una serie de productos que no podrán ser devueltos </font></h4>            
+            <br>
+
+
+           <h3><font color="#DF3A01">¿En que condiciones debe estar el paquete y el empaque?</font></h3>
+           <br>
+           <h4><font color="#585858"><p align="justify">En las mejores condiciones posibles. Es fundamental que conserves el empaque original, pues es tan importante como el producto mismo; cuídalo, guárdalo y manipúlalo con el mayor de los cuidados, de esta forma no tendrás ningún inconveniente al momento de realizar la devolución.
+
+          Una vez que imprimas la guía de la paquetería, es importante no pegarla directamente en la caja original del producto, ya que el pegamento puede dañar el empaque y tu devolución puede ser rechazada. Utiliza otra caja o envoltura para pegar tu guía y proteger el empaque original.
+
+            Por favor, ten en cuenta que si no conservas el empaque original, o si éste se encuentra en mal estado, tu devolución será rechazada.>
+            </font></h4>
+            <br>
+
+            <h3><font color="#DF3A01">¿Puedo pagar con tarjetas internacionales?</font></h3>
+           <br>
+           <h4><font color="#585858"><p align="justify">Sí, en Linio estamos preparados para que puedas pagar fácil y cómodamente. Por ello, contamos con posibilidad de pago mediante tarjetas de crédito o débito internacionales asociadas a Visa, Mastercard y American Express..>
+            </font></h4>
+    
+
+      </div>
+
+      <div class="col-md-2">
+
+      </div>
+
+
+       
+					
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+     
+			
+		</div>
+
+		<div class="col-md-4">
+
+    
+		
+	</div>
+
+	
+	<div class="row">
+		<div class="col-md-12">
+    
+     
+         
+
+
+	    		
+		</div>
+					
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+          <hr>
+     
+		
+		</div>
+
+    <div class="col-md-6">
+   
+
+      </div>
+
+         <div class="col-md-6">
+         
+         
+
+            
+      </div>
+		
+	</div>
+  <hr>
+
+
+	<div class="row">
+		<div class="col-md-12">
+
+	    		<font color="white">col-md-8</font>
+		</div>
+					
+	</div>
+
+
+	<div class="row">
+            <div class="col-md-12">
+           
+
+
+            </div>
+
+
+ </div>
+
+	<div class="row">
+		<div class="col-md-12">
+	    		<BR>
+		</div>
+					
+	</div>
+
+	<?php
+ include 'bottom.html'
+?>
