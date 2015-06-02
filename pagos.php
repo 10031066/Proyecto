3bb35bc8983c8ea9510a3325ef9f48cea1929347
karlit <karlita_erika_07_14@hotmail.com>
2015-06-02 15:23:33 -0500

    pagos.php

---
+++ pagos.php
@@ -0,0 +1,147 @@
+<?php
+ include 'top.php'
+?>
+		
+		
+
+	<div class="row">
+		<div class="col-md-2">
+      <img src="img/public.png" width="300" />
+      <img src="img/public1.png" width="300" />
+      <br>
+      <img src="img/public2.png" width="300" />
+
+
+      </div>
+
+      <div class="col-md-8">
+
+         <h3><font color="#DF3A01">¿Es seguro pagar online con Muebles el Carpintero?</font></h3>
+        <br>
+
+           <h4><font color="#585858"><p align="justify">Sí, en Linio garantizamos al 100% la seguridad de tus pagos. Al momento de pagar con tu tarjeta de crédito o débito, toda tu información es cifrada mediante el mejor sistema de protección Secure Sockets Layer (SSL) de 256-bit. Así que puedes estar tranquilo, nosotros no guardamos ningún dato de tu tarjeta.</P>
+           </font></h4>
+           <br>
+
+           <h3><font color="#DF3A01">¿Puedo pagar en mensualidades?</font></h3>
+           <br>
+
+           <h4><font color="#585858"><p align="justify">Sí, la posibilidad de pagar en mensualidades existe, pero varía de acuerdo con las restricciones de cada compra (monto mínimo, promoción y banco). Recuerda que para esta modalidad, la única forma de pago disponible es con tarjetas de crédito.
+
+            MESES SIN INTERESES
+
+            Con esta opción, podrás diferir el costo de tu producto entre 3, 6, 9, 12 ó 18 mensualidades. Sin embargo, debes tomar en cuenta que el número de meses depende exclusivamente de la promoción y de las tarjetas de crédito participantes.
+
+            Al realizar tu compra, el monto final se dividirá entre los meses de la promoción sin cobrar intereses (con puntualidad de pago). Por favor, no olvides revisar los términos de cada promoción.>
+           </font></h4>
+
+           <br>
+
+           <h3><font color="#DF3A01">¿Que hago si mi pago con tarjeta no funciona?</font></h3>
+           <br>
+           <h4><font color="#585858"><p align="justify">Por favor comprueba que toda la información que ingreses sea correcta. Si el problema continúa, comunícate con tu banco para consultar si existe algún inconveniente con tu cuenta. En ocasiones, el banco puede rechazar una compra por error.
+
+            Recuerda que en Linio contamos con diferentes formas de pago, si deseas que te ayudemos a finalizar tu compra, por favor contáctanos. </font></h4>            
+            <br>
+
+
+           <h3><font color="#DF3A01">¿Las formas de pago aplican para todos los productos y destinos?</font></h3>
+           <br>
+           <h4><font color="#585858"><p align="justify">No, desafortunadamente no aplican para todos los productos ni destinos. Esto puede variar según el monto y la promoción, sin embargo, en Linio siempre tenemos diferentes opciones de pago para que puedas concretar tu compra.>
+            </font></h4>
+            <br>
+
+            <h3><font color="#DF3A01">¿Puedo pagar con tarjetas internacionales?</font></h3>
+           <br>
+           <h4><font color="#585858"><p align="justify">Sí, en Linio estamos preparados para que puedas pagar fácil y cómodamente. Por ello, contamos con posibilidad de pago mediante tarjetas de crédito o débito internacionales asociadas a Visa, Mastercard y American Express..>
+            </font></h4>
+    
+
+      </div>
+
+      <div class="col-md-2">
+
+      </div>
+
+
+       
+					
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+     
+			
+		</div>
+
+		<div class="col-md-4">
+
+    
+		
+	</div>
+
+	
+	<div class="row">
+		<div class="col-md-12">
+    
+     
+         
+
+
+	    		
+		</div>
+					
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+          <hr>
+     
+		
+		</div>
+
+    <div class="col-md-6">
+   
+
+      </div>
+
+         <div class="col-md-6">
+         
+         
+
+            
+      </div>
+		
+	</div>
+  <hr>
+
+
+	<div class="row">
+		<div class="col-md-12">
+
+	    		<font color="white">col-md-8</font>
+		</div>
+					
+	</div>
+
+
+	<div class="row">
+            <div class="col-md-12">
+           
+
+
+            </div>
+
+
+ </div>
+
+	<div class="row">
+		<div class="col-md-12">
+	    		<BR>
+		</div>
+					
+	</div>
+
+<?php
+ include 'bottom.html'
+?>
