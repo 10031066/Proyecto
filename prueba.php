2aac2136bacf58e5ecbba2eb0be6821e396164d9
karlit <karlita_erika_07_14@hotmail.com>
2015-06-02 11:45:26 -0500

    modifique prueba

--- prueba.php
+++ prueba.php
@@ -2,6 +2,14 @@
  include 'top.php'
 ?>
 
+<div class="row">
+    <div class="col-md-12">
+          <font color="white">separacion</font>5
+    </div>
+
+          
+</div>
+
 
 <?php
  include 'bottom.html'
