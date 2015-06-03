2a52a3c0cf3c850e2d434636c234ec5b5fae9443
karlit <karlita_erika_07_14@hotmail.com>
2015-06-03 14:56:37 -0500

    producto.php

---
+++ producto.php
@@ -0,0 +1,100 @@
+<?php
+ include 'top.php'
+?>
+
+<div class="row">
+    <div class="col-md-2">
+      <img src="img/public.png" width="300" />
+      <img src="img/public1.png" width="300" />
+      <br>
+      <img src="img/public2.png" width="300" />
+      <br>
+      <img src="img/public.png" width="300" />
+
+
+      </div>
+
+      <div class="col-md-8">
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <h3 class="panel-title">Nombre del articulo</h3>
+          </div>
+          <div class="panel-body">
+
+          <div class="col-md-3">
+             <h4><font color="#DF3A01">Caracteristicas Principales</font></h4>
+            <P style="text-align: center;">
+                <span style="color:#424242">
+                    Calidad garantizada<br>
+                    Tela en Lino<br>
+                    Viene con 9 cojines <br>
+                    Pata Metálica<br>
+                    <h6><font color="#DF3A01">Mas informacion <button onclick="mostrar()">   
+                    <span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></button></font></h6>
+ 
+               </P>
+        </div>
+
+          <div class="col-md-6">
+            <img src="img/sala1.jpg" width="250" onmouseover="javascript:this.height=900;this.width=900"  
+                   onmouseout="javascript:this.width=250;this.height=250"/>
+
+            <HR><input class="btn btn-primary" type="submit" value="Comprar ahora" >
+          </div>
+
+          <div class="col-md-3">
+           <h4><font color="#DF3A01">Precio</font></h4>
+            <P style="text-align: center;">
+                <span style="color:#424242">$ 4,799<br>
+                 3 pagos desde <br>
+                 $ 1,973.83 
+               </P>
+          </div>
+                 
+
+        </div>
+        </div>
+
+         <div class="panel panel-default">
+  <div class="panel-body">
+    <h4><font color="#DF3A01">Detalles</font></h4>
+<P style="text-align: justify;">
+<span style="color:#088A29"> 
+    Sala Esquinero Greco<br>
+<span style="color:#424242">
+La Sala Esquinero Greco tiene un diseño elegante y sofisticado, lleno de lujo para tu hogar, su diseño cuenta con 2 piezas, 
+un sofa y un love seat, con terminados en lino color gris para que combine con todos los colores y decoraciones de tu casa. 
+La sala Greco que Linio trae para ti esta llena de comodidad y confort para que disfrutes cada uno de los momentos que pases ahí. 
+Llena tu casa de comodidad, de calidad y espacios creativos. <br>
+<span style="color:#088A29"> 
+Medidas<br>
+<span style="color:#424242">
+Sabemos la importancia que ejercen las medidas a la hora de hacer una nueva adquisición y por eso 
+ponemos a tu disposición las medidas de la sala Greco siendo las siguientes:<br>
+<span style="color:#088A29"> 
+Love Seat<br>
+<span style="color:#424242">
+    85cm de Alto<br>
+    134 cm de Largo<br>
+    88 cm de Ancho<br>
+<span style="color:#088A29"> 
+Sofá<br>
+<span style="color:#424242">
+    85cm de Alto<br>
+    200 cm de Largo<br>
+    88 cm de Ancho<br>
+  </div>
+</div>
+
+      </div>
+
+      
+    <div class="col-md-2"></div>
+
+    </div>
+ 
+
+
+<?php
+ include 'bottom.html'
+?>
