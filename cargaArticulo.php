a797680a65264b8f74673d5a4564e25225abd2d3
10031066 <edgar_5_11@hotmail.com>
2015-06-05 14:58:35 -0500

    MuestraArticulos

--- cargaArticulo.php
+++ cargaArticulo.php
@@ -1,7 +1,8 @@
 <?php
 $resultado = ejecutaQuery($query);
-		$fila = $resultado->fetch_array(MYSQLI_ASSOC);
+		
 for($i=0;$i<$resultado->num_rows;$i++){
+	$fila = mysqli_fetch_array($resultado);
 ?>
 <div class="row">
 
@@ -10,30 +11,25 @@ for($i=0;$i<$resultado->num_rows;$i++){
               <div class="panel-body">
 
                 <div class="col-md-12">
-                   <a href="Producto.php"><img src="img/sala1.jpg" width="250" onmouseover="javascript:this.height=500;this.width=500"  
+                   <a href="Producto.php"><img src="img/articulos/<?php  echo $fila['id']; ?>.jpg" width="250" onmouseover="javascript:this.height=500;this.width=500"  
                    onmouseout="javascript:this.width=250;this.height=250"/>
               	</div>
 
               	<div class="col-md-12">
-                   <h4><font color="#DF3A01"><?php echo $resultado[$i]['marca']?></font></h4>
+                   <h4><font color="#DF3A01"><?php echo $fila['marca'];?></font></h4>
       				<HR >
 
       				<P style="text-align: center;">
-			          <span style="color:#424242">$ <?php $fila[$i]['precio']?><br> 
+			          <span style="color:#424242">$ <?php echo $fila['precio']; ?><br> 
 			         </P>
 
 			         <br><input class="btn btn-primary" type="submit" value="Comprar ahora" >
 
               	</div>
-
-
               </div>
             </div>
-
             <HR>
-
     </div>
-          
 </div>
 
 <?php } ?>
